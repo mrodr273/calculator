2e1b5bddfe04563c86bbf28c05b5f5ac6d40f00a
Maria Concepcion Rodriguez and Neil Bateman <neil@>
2019-01-30 20:47:09 -0800

    added more scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+/*var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var multi = multiply(number1, number2);
+alert(multi);
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -6,3 +24,28 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
+var add = function(number1, number2) {
+  return number1 + number2;
+;
+
+ar number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(multi);
+
+/*
+var BMI = function(weight, height) {
+return weight/height
+}
+ var weight = parseInt(prompt("What's your weight?"));
+ var height = parseInt(prompt("What's your height?"));
+ alert("Your BMI = "+BMI(34, 434));
+ */
+
+ var celsius = function(F) {
+   var result=(F-32)*5/9;
+   return result
+ }
+var c = parseInt(prompt("What's the Temp. (F)?"));
+ alert("The temperture is (c) "+celsius(c));
